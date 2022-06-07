fe9cdb9ff01a10820735d275900f09c3ea0af954
Fajar Zuhri Hadiyanto <52820619+fajarzuhrihadiyanto@users.noreply.github.com>
2022-06-07 21:39:05 +0700

    Create TrashTypes.sql

---
+++ dml/TrashTypes.sql
@@ -0,0 +1,5 @@
+INSERT INTO TrashTypes VALUES ('Glass', 210, 3150);
+INSERT INTO TrashTypes VALUES ('E-Waste', 350, 5250);
+INSERT INTO TrashTypes VALUES ('Metal', 350, 5250);
+INSERT INTO TrashTypes VALUES ('Paper', 100, 1500);
+INSERT INTO TrashTypes VALUES ('Plastic', 175, 2625);
